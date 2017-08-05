418dafac89b3eb5d6bb23b1856caedbf5100e94e
Dane Laban <Dane Laban>
2017-08-05 21:12:37 +1000

    Added archiving to S3

---
+++ S3Archive.py
@@ -0,0 +1,58 @@
+import os
+import tarfile
+import datetime
+from io import BytesIO
+
+import boto3
+
+
+class S3Archive:
+    def __init__(
+        self,
+        archive_name,
+        bucket_name,
+        add_datetime_to_name=True,
+    ):
+        self.bucket_name = bucket_name
+
+        if add_datetime_to_name:
+            time_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            archive_name += time_now
+        self.archive_name = archive_name + '.tar'
+
+        self.files_for_archive = []
+
+    def addfile(
+        self,
+        file_location
+    ):
+        if os.path.isfile(file_location):
+            self.files_for_archive.append(file_location)
+        else:
+            raise(FileNotFoundError)
+
+    def __makearchive( 
+        self
+    ):
+        archive_in_ram = BytesIO()
+        with tarfile.open(fileobj=archive_in_ram, mode='w:gz') as tar_file:
+            for fname in self.files_for_archive:
+                tar_file.add(name=fname)
+            tar_file.close()
+
+        archive_in_ram.seek(0)  # <--- reset your cursor
+
+        print("Archive written with {} files.".
+              format(len(self.files_for_archive))
+              )
+        return archive_in_ram
+
+    def create(
+        self
+    ):
+        archive = self.__makearchive()
+
+        s3 = boto3.resource('s3')
+        print("Starting upload to S3 bucket: " + self.bucket_name)
+        s3.Object(self.bucket_name, self.archive_name).put(Body=archive.read())
+        print("Finished upload of " + self.archive_name)
--- scrape_google.py
+++ scrape_google.py
@@ -10,37 +10,48 @@ Pre-requisties:
 
 import requests
 import itertools
+import os
+
+from S3Archive import S3Archive
+
 
 def main():
-	# Google static maps API key must be stored in api_key.txt
-	api_key = open('api_key.txt', 'r').read()
-	
-	# Define start positions for the lat and log
-	lat_start = -27.458462
-	long_start = 153.035735
-	# Define the lat and long increments - this depends on the start lat and long
-	lat_inc = -0.0007
-	long_inc = 0.0009
+    s3archive = S3Archive(
+        archive_name='GoogleMapsImages',
+        bucket_name='zepto-archive',
+        add_datetime_to_name=True
+    )
+    # Google static maps API key must be stored in api_key.txt
+    api_key = open('api_key.txt', 'r').read()
 
-	# Iterate over a 10x10 grid and capture satellite images
-	for i, j in itertools.product(range(10), range(10)):
-		latitude = lat_start + (i * lat_inc)
-		longitude = long_start + (j * long_inc)
-		
-		# Generate the Google maps URL 
-		map_address = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom=20&size=640x640&maptype=satellite&key={}'.format(
-						latitude, longitude, api_key
-						)
+    # Define start positions for the lat and log
+    lat_start = -27.458462
+    long_start = 153.035735
+    # Define the lat and long increments - this depends on the start lat and long
+    lat_inc = -0.0007
+    long_inc = 0.0009
 
-		# Specify the filename to save the png to in the google_image_dump folder
-		img_fname = 'google_image_dump\{}{}.png'.format(
-						i, j)
+    # Iterate over a 10x10 grid and capture satellite images
+    for i, j in itertools.product(range(10), range(10)):
+        latitude = lat_start + (i * lat_inc)
+        longitude = long_start + (j * long_inc)
 
-		# Fetch the image at the URL and save it to the filename
-		f = open(img_fname, 'wb')
-		f.write(requests.get(map_address).content)
-		f.close()
+        # Generate the Google maps URL
+        map_address = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom=20&size=640x640&maptype=satellite&key={}'.format(
+                        latitude, longitude, api_key
+                        )
+
+        # Specify the filename to save the png to in the google_image_dump folder
+        img_fname = os.path.join('google_image_dump', '{}{}.png'.format(i, j))
+
+        # Fetch the image at the URL and save it to the filename
+        with open(img_fname, 'wb') as f:
+            f.write(requests.get(map_address).content)
+
+        s3archive.addfile(img_fname)
+
+    s3archive.create()
 
 if __name__ == "__main__":
-	# execute only if run as a script
-	main()
+    # execute only if run as a script
+    main()
