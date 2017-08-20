cdf0f38fff70377dde05a6424ab4291314cc04e6
Dane Laban <Dane Laban>
2017-08-21 00:24:28 +1000

    Modified car selection to two diagonal car extremities

--- car_clicker.py
+++ car_clicker.py
@@ -25,40 +25,66 @@ def carclicker(fname, scale=2):
 
     coords = []
     coord_rects = []
-    temp_rect = None
+    first_coord = []
+    temp_shapes = []
 
-    def drawbox(x, y, colour='white'):
+    def drawbox(x, y, colour='white', width=1, size=80):
         # Draws a rectangle around the x, y position
         nonlocal canvas
-        size = 80
         x0 = x - size//2
         x1 = x0 + size
         y0 = y - size//2
         y1 = y0 + size
-        id = canvas.create_rectangle(x0, y0, x1, y1, width=2, outline=colour)
+        id = canvas.create_rectangle(x0, y0, x1, y1, width=width, outline=colour)
         return id
 
-    def tempbox(event):
-        nonlocal canvas, temp_rect
-        x = event.x
-        y = event.y
-        # Remove the last rectangle
-        if temp_rect is not None:
-            canvas.delete(temp_rect)
-        # Draw the new rectangle
-        temp_rect = drawbox(x, y, 'white')
+    def drawcircle(x, y, colour='white', width=1, size=80):
+        # Draws a circle around the x, y position
+        nonlocal canvas
+        x0 = x - size//2
+        x1 = x0 + size
+        y0 = y - size//2
+        y1 = y0 + size
+        id = canvas.create_oval(x0, y0, x1, y1, width=width, outline=colour)
+        return id
+
+    def tempshape(event):
+        nonlocal canvas, temp_shapes, first_coord
+        x1 = event.x
+        y1 = event.y
+        # Log the first coord
+        if not first_coord:
+            first_coord.append(x1)
+            first_coord.append(y1)
+        # Remove the last shape(s)
+        if temp_shapes:
+            for shape in temp_shapes:
+                canvas.delete(shape)
+        # Draw the new shape(s)
+        x0 = first_coord[0]
+        y0 = first_coord[1]
+        x = (x0 + x1) / 2
+        y = (y0 + y1) / 2
+        size = ((x0-x1)**2 + (y0-y1)**2)**0.5
+        temp_shapes.append(drawcircle(x, y, colour='white', size=size))
 
     def logcoords(event):
         # Append the x, y click coords to coords list
-        nonlocal coords, coord_rects, temp_rect, scale
-        x = event.x
-        y = event.y
+        nonlocal coords, coord_rects, temp_shapes, first_coord, scale
+        x1 = event.x
+        y1 = event.y
+        x0 = first_coord[0]
+        y0 = first_coord[1]
+        x = (x0 + x1) // 2
+        y = (y0 + y1) // 2
         # Remove the temporary rectangle
-        if temp_rect is not None:
-            canvas.delete(temp_rect)
-            temp_rect = None
+        if temp_shapes:
+            for shape in temp_shapes:
+                canvas.delete(shape)
+            temp_shapes = []
+            first_coord = []
         # Draw a rectangle around the coords
-        rect_id = drawbox(x, y, 'green')
+        rect_id = drawbox(x, y, colour='green', width=2)
         # Scale x and y coords to row column of original image
         column = x // scale
         row = y // scale
@@ -75,12 +101,13 @@ def carclicker(fname, scale=2):
         canvas.delete(rect_id)
 
     # Bind left click motion to draw a temporary rectangle
-    canvas.bind("<B1-Motion>", tempbox)
+    canvas.bind("<B1-Motion>", tempshape)
     # Bind release left click to log the coord
     canvas.bind("<ButtonRelease-1>", logcoords)
     # Bind right click to remove the last coord
     canvas.bind("<Button-3>", removelastcoord)
 
+    root.geometry("+10+10")  # Open window 10px offset from top left corner
     root.mainloop()
 
     return coords
@@ -91,7 +118,7 @@ def main():
 
     img_files = glob.glob('google_image_dump/*.png')
 
-    for img_file in img_files:
+    for img_file in img_files[:1]:
         coords = carclicker(img_file)
 
         dat_file = img_file + '.dat'
@@ -101,11 +128,11 @@ def main():
 
         dat_file_list.append(dat_file)
 
-    archive = Archive(dat_file_list)
-    buffer = archive.streamtargz()
+    # archive = Archive(dat_file_list)
+    # buffer = archive.streamtargz()
 
-    s3bucket = S3Bucket('zepto-archive')
-    s3bucket.uploadstream(buffer, archive.name)
+    # s3bucket = S3Bucket('zepto-archive')
+    # s3bucket.uploadstream(buffer, archive.name)
 
 
 if __name__ == "__main__":
